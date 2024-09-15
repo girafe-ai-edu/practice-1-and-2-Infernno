1fa6c1c2ea3c210e49caed5580bca1e4c7e52d28
Sviridov Vladislav <sviridov.vs@phystech.edu>
2024-09-15 17:49:56 +0700

    Do practice 2

--- practice_2/task_3.py
+++ practice_2/task_3.py
@@ -3,9 +3,21 @@
 Write a program to calculate the body mass index (BMI) of a person. The user must enter their height and weight, after which you use one of the formulas below to determine the index.
 BMI = weight/height**2 
 """
-weight = input()
-height = input()
 
+def readFloat(hint):
+    try:
+        print(hint)
+        val = float(input())
+        print()
+        return val
+    except ValueError:
+        return None
 
-#code here
+weight = readFloat("Enter your weight in kg:")
+height = readFloat("Enter your height in m:")
 
+if weight is not None and height is not None:
+    bmi = weight/height**2
+    print(f'Your BMI is {bmi:.2f} kg/m^2')
+else:
+    print('Failed to compute BMI, please check your input.')
--- practice_2/task_4.py
+++ practice_2/task_4.py
@@ -4,3 +4,39 @@ Develop a program that asks the user for an integer 4-digit number and calculate
 3 + 1 + 4 + 1 = 9
 
 """
+
+
+def readInt(hint):
+    try:
+        print(hint)
+        val = int(input())
+        print()
+        return val
+    except ValueError:
+        return None
+
+
+number = readInt("Enter your number:")
+
+if number is None:
+    print("Failed to read number")
+    exit()
+
+if number < 1000 or number > 9999:
+    print("Number should contain only 4 digits")
+    exit()
+
+x = number
+y = 0
+
+digits = []
+
+while x > 0:
+    digit = x % 10
+    y += digit
+    x //= 10
+    digits.append(str(digit))
+
+digits.reverse()
+
+print(f'{' + '.join(digits)} = {y}')
